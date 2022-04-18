ef945843d29e70537b1bb9e3fb6115f38212a544
Marco Antônio Patrocinio <73245980+MarcoPatrocinio@users.noreply.github.com>
2022-04-18 01:09:50 -0300

    ..

--- src/components/templates/AirInfo/AirInfo.jsx
+++ src/components/templates/AirInfo/AirInfo.jsx
@@ -5,17 +5,17 @@ import { MdOutlineEqualizer } from "react-icons/md";
 export default (props) => (
   <div className="air_info_content">
     <div className="air_info_img">
-      <img src={props.icon} width={35} height={35} />
       <span style={{ fontSize: "0.625rem" }}>{props.name}</span>
-      <div className="air_info_value">
-        <span>
+      <svg width="51" height="51" style={{borderRadius: "50%"}}>
+        <circle r="25.5" cx="25.5" cy="25.5" transform="rotate(90 25.5 25.5)" style={{strokeWidth: "10px", fill: "rgba(255, 255, 255, 0.8)"}}></circle>
+        <circle r="25.5" cx="25.5" cy="25.5" transform="rotate(90 25.5 25.5)" style={{strokeWidth: "10px", strokeDasharray: "14.407075 396.442451", stroke: "#00E89D", fill: "rgba(172,172,172,0.25"}}></circle>
+        <text x="35%" y="55%">50</text>
+      </svg>
+      <span>
           {props.value} <span style={{ fontSize: "0.625rem" }}>μg/m3</span>
-        </span>
+      </span>
+      <div className="air_info_value">
       </div>
-    </div>
-    <div className="el-rating">
-      <MdOutlineEqualizer size={30} color={props.quality.hex} />
-      <span>{props.quality.msg}</span>
     </div>
   </div>
 );
--- src/components/templates/Wind/Wind.jsx
+++ src/components/templates/Wind/Wind.jsx
@@ -5,22 +5,27 @@ import AirInfo from "../AirInfo/AirInfo";
 let notfound = "https://icon-library.com/images/not-found-icon/not-found-icon-24.jpg"
 export default props =>
 <div className="wind cards-inicio-informacoes">
-    <div className="wind_main">
-        <div>
-           <span>Velocidade: {props.wind.speed}</span> 
+    <h1>Vento e Ar</h1>
+    <div>
+        <h4>Velocidade e Direção</h4>
+        <div className="wind_main">
+            <div>
+                <span>Velocidade: {props.wind.speed}</span> 
+            </div>
+            <div>
+                <span>Direção: {props.wind.deg}º</span> 
+                
+            </div>
         </div>
-        <div>
-           <span>Direção: {props.wind.deg}º</span> 
+        <hr />
+        <h4>Componentes</h4>
+        <div className="wind_components">
+            <AirInfo name="Carbono"value={props.components.co} icon={notfound} quality={{hex: "#000", msg: "Ruim"}}/>
+            <AirInfo name="Dióxido de nitrogênio" value={props.components.no2} icon={notfound} quality={{hex: "#fff", msg: "Ruim"}}/>
+            <AirInfo name="Ozônio" value={props.components.o3} icon={notfound} quality={{hex: "#fff", msg: "Ruim"}}/>
+            <AirInfo name="Dióxido de enxofre" value={props.components.so2} icon={notfound} quality={{hex: "#fff", msg: "Ruim"}}/>
+            <AirInfo name="2.5 Microns" value={props.components.pm2_5} icon={notfound} quality={{hex: "#fff", msg: "Ruim"}}/>
+            <AirInfo name="10 Microns" value={props.components.pm10} icon={notfound} quality={{hex: "#fff", msg: "Ruim"}}/>
         </div>
-    </div>
-    <hr />
-    <h4>Componentes</h4>
-    <div className="wind_components">
-        <AirInfo name="Carbono"value={props.components.co} icon={notfound} quality={{hex: "#000", msg: "Ruim"}}/>
-        <AirInfo name="Dióxido de nitrogênio" value={props.components.no2} icon={notfound} quality={{hex: "#fff", msg: "Ruim"}}/>
-        <AirInfo name="Ozônio" value={props.components.o3} icon={notfound} quality={{hex: "#fff", msg: "Ruim"}}/>
-        <AirInfo name="Dióxido de enxofre" value={props.components.so2} icon={notfound} quality={{hex: "#fff", msg: "Ruim"}}/>
-        <AirInfo name="2.5 Microns" value={props.components.pp2_5} icon={notfound} quality={{hex: "#fff", msg: "Ruim"}}/>
-        <AirInfo name="10 Microns" value={props.components.p10} icon={notfound} quality={{hex: "#fff", msg: "Ruim"}}/>
     </div>
 </div>
